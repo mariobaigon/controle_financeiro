56391de8edd2a9dd9da950555aaea573b9a01663
mariobaigon <50970427+mariobaigon@users.noreply.github.com>
2023-04-17 07:45:36 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import GlobalStyle from './styles/global';
+import { Header } from './components/Header';
+import { Resume } from './components/Resume';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Resume/>
+      <Form/>
+      <GlobalStyle/>
+    </>    
+  )
+}
+export default App;
+
+
+
